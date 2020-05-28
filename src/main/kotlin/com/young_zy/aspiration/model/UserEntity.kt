157a3dd7b35df4fc932e7f3c49559b95a04f8523
young-zy <youngzhaozhou@gmail.com>
2020-05-29 00:09:38 +0800

    update user entity schema

--- src/main/kotlin/com/young_zy/aspiration/model/UserEntity.kt
+++ src/main/kotlin/com/young_zy/aspiration/model/UserEntity.kt
@@ -1,12 +1,17 @@
 package com.young_zy.aspiration.model
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("user")
 data class UserEntity(
-    var uid: Int,
-    var username: String,
-    var realName: String,
-    var hashedPassword: String,
-    var auth: String,
-    var region: String,
-    var gender: String,
-    var age: Int
+        @Id
+        var uid: Int? = null,
+        var username: String,
+        var realName: String,
+        var hashedPassword: String,
+        var auth: String,
+        var region: String,
+        var gender: String,
+        var age: Int
 )
