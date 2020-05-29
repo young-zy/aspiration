36d14432183f2bfc679115bd771d0c02fb928fff
young-zy <youngzhaozhou@gmail.com>
2020-05-29 22:54:23 +0800

    remove age from UserInfoUpdateRequest schema

--- src/main/kotlin/com/young_zy/aspiration/controller/request/UserInfoUpdateRequest.kt
+++ src/main/kotlin/com/young_zy/aspiration/controller/request/UserInfoUpdateRequest.kt
@@ -1,7 +1,6 @@
 package com.young_zy.aspiration.controller.request
 
 data class UserInfoUpdateRequest(
-        val age: Int,
         val password: String,
         val newPassword: String?,
         val newAge: Int?,
